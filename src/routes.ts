5dc78adbdf65c88dd5352ded6691c7881e5ed4c9
Bianca <biiafranca@hotmail.com>
2021-10-17 19:27:00 -0300

    chore: implementaçao autenticaçao do usuario

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import {Router, Request, Response, response} from 'express';
 import {ActivityController} from './controller/ActivityController';
 import {CourseUnitController} from './controller/CourseUnitController';
 import {UserController} from './controller/UserController';
+import {AuthenticateController} from './controller/AuthenticateController'
 
 interface UserRequest {
     name:string;
@@ -12,13 +13,13 @@ interface UserRequest {
 const userController = new UserController();
 const activityController = new ActivityController();
 const courseUnitController = new CourseUnitController();
+const authenticateController = new AuthenticateController();
 
 const routes = Router();
 
 routes.post('/user', userController.create);
-
+routes.post('/auth', authenticateController.create);
 routes.post('/activity', activityController.create);
-
 routes.post('/courseunit', courseUnitController.create);
 
 export default routes;
