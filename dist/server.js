dd810e22486a36a3ab2ac25466ea755e4e9d9067
Bianca <biiafranca@hotmail.com>
2021-11-16 21:21:02 -0300

    fix: error Cannot find module 'dotenv/config'

--- dist/server.js
+++ dist/server.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 exports.__esModule = true;
-require("dotenv/config");
+// require("dotenv/config");
 var express_1 = __importDefault(require("express"));
 var cors_1 = __importDefault(require("cors"));
 require("./database");
